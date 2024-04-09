7b7fe8718dd936812d14b9a2ba6191f12929f035
Anton <skorodeev@gmail.com>
2024-04-09 16:04:37 +0300

    pytest add

---
+++ test_all_tasks.py
@@ -0,0 +1,33 @@
+from teccod_interview import (task1, task2, task4, Point)
+
+
+def test_task1():
+    assert task1([1, 2, 3, 3, 7]) == [1, 2, 3, 7]
+
+
+def test_task2():
+    assert task2(13, 50) == [13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    assert task2(50, 13) == [13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    assert task2(13, 47) == [13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    assert task2(10, 50) == [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    assert task2(47, 47) == [47]
+    assert task2(50, 50) == []
+
+
+def test_task3():
+    point_a = Point(77, 89, 'Pt_A')
+    point_b = Point(81, 92, 'Pt_B')
+    assert point_a.dist_to_point(point_b) == 5
+    assert point_a.get_coords() == (77, 89)
+    assert point_a.set_coords(100, 100) is True
+    assert point_b.set_coords(110, 100) is True
+    assert point_a.dist_to_point(point_b) == 10
+    assert str(point_a) == "Pt_A: X=100 Y=100"
+
+
+def test_task4():
+    print(task4(['abc', 'ab', 'abcdef', 'a']))
+    print(task4(['abc', 'ab', 'abcdef', 'a'], False))
+    assert task4(['abc', 'ab', 'abcdef', 'a']) == ['a', 'ab', 'abc', 'abcdef']
+    assert task4(['abc', 'ab', 'abcdef', 'a'], False) == [
+        'abcdef', 'abc', 'ab', 'a']
